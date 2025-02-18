03dfc18df4907170af806b87548b7a20b44ebfb4
keynard <keynardamacna@gmail.com>
2025-02-18 23:32:09 +0800

    ugma napud

---
+++ src/users/user.interface.ts
@@ -0,0 +1,16 @@
+export interface User{
+username: string;
+email: string;
+password: string;
+}
+
+export interface UnitUser extends User{
+    id: string;
+}
+
+export interface Users {
+    [key: string]: UnitUser;
+}
+
+
+
